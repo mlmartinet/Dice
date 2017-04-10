b61050fe9dd4960c0a71ca3e6689e1a5aa9241e9
Matthew Martin <mlmartin12480@gmail.com>
2017-04-09 21:35:26 -0400

    Add files via upload
    added a pin defines for practice and attempted to display random numbers using rand() function only shows a 2 and a 6.

--- dice.c
+++ dice.c
@@ -6,23 +6,47 @@
 // ------- Preamble -------- //
 #include <avr/io.h>                        /* Defines pins, ports, etc */
 #include <util/delay.h>                     /* Functions to waste time */
+#include "pindefine.h"
+#include "stdlib.h"
 
+uint8_t i = 0;
+uint8_t dice[6]={0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D};
+int die1;
+int die2;
 
-int main(void) {
+rand(void);
+
+int main(void){
 
   // -------- Inits --------- //
-  //Use all bits in DDRB as outputs
-  DDRB = 0xff;    
-  //All Segments on
-  PORTB = 0xFF;                             
+  //Set outputs
+  DDRB = 0x7F;
+  DDRD = 0x7F;
+  die1 = rand() %6;
+  die2 = rand() %6;
+
 
   // ------ Event loop ------ //
-  while (1) {
-   
-    //just a lazy countown test to play with code.
-    PORTB = PORTB;
-    _delay_ms(1000);
-    PORTB = PORTB -1;
-  }                                                  /* End event loop */
-  return 0;                            /* This line is never reached */
+
+  while (1)
+	  {
+
+	  while(i < 10){
+		  DICE1 |= 0x7f;
+	   	  _delay_ms(75);
+	   	  DICE2 |= 0x7F;
+ 	  	  _delay_ms(75);
+	  	  DICE2 = ~DICE2;
+	  	  _delay_ms(75);
+ 	  	  DICE1 = ~DICE1;
+ 	  	  _delay_ms(75);
+ 	  	  i ++;
+	  }
+
+	  DICE1 = dice[die1];
+	  DICE2 = dice[die2];
+
+	  }                                                  /* End event loop */
+  
+  return 0;                            				/* This line is never reached */
 }
---
+++ pindefine.h
@@ -0,0 +1,15 @@
+/*
+ * pindefine.h
+ *
+ *  Created on: Apr 8, 2017
+ *      Author: mlmartin
+ */
+
+#ifndef PINDEFINE_H_
+#define PINDEFINE_H_
+
+#define DICE1 PORTB
+#define DICE2 PORTD
+
+
+#endif /* PINDEFINE_H_ */
